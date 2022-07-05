c90af3b56fbe6b712c36152396209cc410348aaf
Kristina Faye <kglinoga@gmail.com>
2022-07-04 20:20:21 -0700

    added comment in JS to add the current day to the JumboTron

--- assets/script.js
+++ assets/script.js
@@ -45,6 +45,9 @@ var eventFormInput = $('.events-input')
     // shoppingFormEl.on('submit', handleFormSubmit);
             // but we'd have to change it so the FORM itself displays the text, not create a new display (OR if we do create a new display, it'll have to include an if statement/event listener for aclick when the display form is on) 
 
+
+    // display current day in the JumboTron 
+
     // change colors based on past, present or future- I think these are just jQuery forms (see above line)
         // let's try the following: setting an attribute (in JS declare a var that selects all the cells of this column)(use thatVar.attr('name of property', 'value'))(OR try .css(bg for background color??, the color vars given in the stylesheet.))
     // Once there's a method that changes the color of the blocks, we'll set it to an event listener(?OROROROR an if statment) that follows the time. maybe, if hour<=moment.js(something) {color: gray}  OR if hour===moment.js(Something) {color green} OR if hour>=moment.js {color red} ..orsomething like that.
