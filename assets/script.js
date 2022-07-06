5fa413690a7d472bba8ad4883fb0575b4c6d9a93
Kristina Faye <kglinoga@gmail.com>
2022-07-05 19:22:41 -0700

    scaled HTML to all biz hours (9am to 5pm)

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 //first column will just display the hours availalbe (9am, 10am, 11am...) we COULD use a for loop iteration thing to populate all 9 boxes but.... not sure if I wannna lol.  Might be nicer  to just copy/paste this stuff by hand... MIGHT be. 
 var hourBox = $('.hour');
-hourBox.text(9); 
+// hourBox.text("9 AM"); 
+var hourTime9 = $('#9');
+var hourTime10 = $('#10');
+// hourTime10.text("10 AM");
 
- 
 var eventForm = $('.events');
 var eventFormInput = $('input[name="events-input"]');
 var saveButton = $('.saveBtn');
@@ -13,30 +15,22 @@ var eventDisplay = localStorage.getItem("eventCopy");
 saveButton.on("click", function() {
     localStorage.setItem("eventCopy", eventFormInput.val());
     console.log(eventFormInput.val());
-})
-
-
-
+})    
     
-        
-    
-        
-     
-            // Once there's a method that changes the color of the blocks, we'll set it to an event listener(?OROROROR an if statment) that follows the time. maybe, if hour<=moment.js(something) {color: gray}  OR if hour===moment.js(Something) {color green} OR if hour>=moment.js {color red} ..orsomething like that.
             var today = moment();
             $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
             
             // color change: 
             
             function time() {
-                if (hourBox.text < moment().format("k")){
+                if (hourTime9.toInt < moment().format("k")){
                     console.log("boom");
                     eventForm.attr("class", "past col-8");    
                 }
-                else if (hourBox.text > moment().format("k")){
+                else if (hourTime9 > moment().format("k")){
                     eventForm.attr("class", "future col-8");
                 }
-                else if (hourBox.text = moment().format("k")) {
+                else if (hourTime9 = moment().format("k")) {
                     eventForm.attr("class", "present col-8");
                 }
             }
